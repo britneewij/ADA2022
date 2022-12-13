2d6495a2a30a0a95c6dbab608f4406f2b7cd3e90
brit <70126665+britneewij@users.noreply.github.com>
2022-12-14 00:19:19 +0800

    MAX-3-CNF-SAT

--- minihw6.cpp
+++ minihw6.cpp
@@ -19,54 +19,66 @@
 using namespace std;
 #define int long long 
 
-const int MAXX = 1000001;
-typedef pair<int, int> edge;
-vector<edge> adj[MAXX];
-int* inMST = new int[MAXX];
-
-void prim(int x){
-    priority_queue<edge, vector<edge>, greater<edge>> pq;
-    pq.push(make_pair(0, x));
-    edge p;
-
-    int y;
-    int min = 0;
-    while(!pq.empty()){
-        p = pq.top();
-        pq.pop();
-        x = p.second;
-
-        if(inMST[x]==1){
-            continue;
-        }
-        inMST[x] = 1;
-
-        for(int i=0; i<adj[x].size(); ++i){
-            y = adj[x][i].second;
-
-            if(inMST[y]==0){
-                pq.push(adj[x][i]);
-            }
-        }
-        min += p.first;
-    }
-
-    cout << min << "\n";
-}
-
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n, m;
     cin >> n >> m;
 
-    int node1, node2, cost;
+    int *a = new int[m+1];
+    int *b = new int[m+1];
+    int *c = new int[m+1];
     for (int i=0; i<m; i++){
-        cin >> node1 >> node2 >> cost;
-
-        adj[node1].push_back(make_pair(cost, node2));
-        adj[node2].push_back(make_pair(cost, node1));
+        cin >> a[i] >> b[i] >> c[i];
     }
 
-    prim(1);
+    int *v = new int[n+1];
+    for (int i=0; i<n+1; i++){
+        v[i] = rand()%2;
+    }
+
+    for(int i=0; i<m; i++){
+        int va = v[abs(a[i])];
+        if (a[i]<0){
+            va = 1 - va;
+        } 
+        int vb = v[abs(b[i])];
+        if (b[i]<0){
+            vb = 1 - vb;
+        } 
+        int vc = v[abs(c[i])];
+        if (c[i]<0){
+            vc = 1 - vc;
+        } 
+
+        while((va or vb or vc) == 0){
+            if(((1 - va) or vb or vc) == 1){
+                v[abs(a[i])] = 1 - v[abs(a[i])];
+                break;
+            }
+            else if((va or (1 - vb) or vb) == 1){
+                v[abs(b[i])] = 1 - v[abs(b[i])];
+                break;
+            }
+            else if ((va or vb or (1 - vc)) == 1){
+                v[abs(c[i])] = 1 - v[abs(c[i])];
+                break;
+            }
+        }  
+    }
+
+    for (int i=1; i<n+1; i++){
+        if (v[i]==0){
+            cout << "-1";
+        }
+        else cout << "1";
+
+        if (i!=n){
+            cout << " ";
+        }
+        else{
+            cout << "\n";
+        }
+    }
+
     return 0;
 }
